9d223801a146b6a6bd62c53aa6a7d7927e5f9ac8
Angel <angelgaelguevara@gmail.com>
2025-10-20 18:04:41 -0600

    feat: refactor VerifyToken middleware for improved token verification and error handling

--- src/api/middlewares/VerifyToken.ts
+++ src/api/middlewares/VerifyToken.ts
@@ -1,3 +1,21 @@
-import token from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+const secretKey = process.env.JWT_SECRET_KEY;
 
+const verifyToken = (req: Request, res: Response, next: NextFunction): any => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: "Token requerido" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secretKey as string);
+    res.locals.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({ msg: "Token inválido" });
+  }
+};
+
+export default verifyToken;
